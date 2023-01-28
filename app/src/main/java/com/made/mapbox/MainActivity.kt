d5593a5025d2667bfa74560a40266b7ae094f31f
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-01-28 20:48:57 +0700

    add LatLng Location

--- app/src/main/java/com/made/mapbox/MainActivity.kt
+++ app/src/main/java/com/made/mapbox/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.made.mapbox
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val ICON_ID = "ICON_ID"
+    }
 
     private lateinit var mapView: MapView
     private lateinit var btnNavigation: Button
     private lateinit var mapboxMap: MapboxMap
+    private lateinit var symbolManager: SymbolManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +34,36 @@ class MainActivity : AppCompatActivity() {
         mapView.getMapAsync { mapboxMap ->
             this.mapboxMap = mapboxMap
             mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+                symbolManager = SymbolManager(mapView, mapboxMap, style)
+                symbolManager.iconAllowOverlap = true
+
+                style.addImage(
+                    ICON_ID,
+                    BitmapFactory.decodeResource(resources, com.mapbox.mapboxsdk.R.drawable.mapbox_marker_icon_default)
+                )
+
+                showLocation()
             }
         }
+    }
+
+    private fun showLocation() {
+        val location = LatLng(-6.8957643, 107.6338462)
+        symbolManager.create(
+            SymbolOptions()
+                .withLatLng(LatLng(location.latitude, location.longitude))
+                .withIconImage(ICON_ID)
+                .withIconSize(1.5f)
+                .withIconOffset(arrayOf(0f, -1.5f))
+                .withTextField("Location")
+                .withTextHaloColor("rgba(255, 255, 255, 100)")
+                .withTextHaloWidth(5.0f)
+                .withTextAnchor("top")
+                .withTextOffset(arrayOf(0f, 1.5f))
+                .withDraggable(true)
+        )
+
+        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 8.0))
     }
 
     override fun onStart() {
