a218ffca2fc954951697bb884a7737be71b3e7c9
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2023-01-28 20:57:43 +0700

    add request permission

--- app/src/main/java/com/made/mapbox/MainActivity.kt
+++ app/src/main/java/com/made/mapbox/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.made.mapbox
 
+import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.view.animation.BounceInterpolator
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
@@ -22,6 +33,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnNavigation: Button
     private lateinit var mapboxMap: MapboxMap
     private lateinit var symbolManager: SymbolManager
+    private lateinit var locationComponent: LocationComponent
+    private lateinit var myLocation: LatLng
+    private lateinit var permissionManager: PermissionsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +57,47 @@ class MainActivity : AppCompatActivity() {
                 )
 
                 showLocation()
+                showMyLocation(style)
             }
+        }
+    }
+
+    @SuppressLint("Missing Permission")
+    private fun showMyLocation(style: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponentOptions = LocationComponentOptions.builder(this)
+                .pulseEnabled(true)
+                .pulseColor(Color.BLUE)
+                .pulseAlpha(.4f)
+                .pulseInterpolator(BounceInterpolator())
+                .build()
+            val locationComponentActivationOptions = LocationComponentActivationOptions
+                .builder(this, style)
+                .locationComponentOptions(locationComponentOptions)
+                .build()
+            locationComponent = mapboxMap.locationComponent
+            locationComponent.activateLocationComponent(locationComponentActivationOptions)
+            locationComponent.isLocationComponentEnabled = true
+            locationComponent.cameraMode = CameraMode.TRACKING
+            locationComponent.renderMode = RenderMode.COMPASS
+            myLocation = LatLng(locationComponent.lastKnownLocation?.latitude as Double, locationComponent.lastKnownLocation?.longitude as Double)
+            mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12.0))
+        } else {
+            permissionManager = PermissionsManager(object : PermissionsListener {
+                override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+                    Toast.makeText(this@MainActivity, "Anda harus mengizinkan location permission untuk menggunakan aplikasi ini", Toast.LENGTH_SHORT).show()
+                }
+                override fun onPermissionResult(granted: Boolean) {
+                    if (granted) {
+                        mapboxMap.getStyle { style ->
+                            showMyLocation(style)
+                        }
+                    } else {
+                        finish()
+                    }
+                }
+            })
+            permissionManager.requestLocationPermissions(this)
         }
     }
 
